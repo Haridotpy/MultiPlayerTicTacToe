d70a6051e7bc0950eaa3daef31be9f79d8183f05
abhi-13-07 <abhishek.13.july.02@gmail.com>
2022-05-03 20:34:55 +0530

    import components from index.ts

---
+++ client/src/Components/index.ts
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const Header = React.lazy(() => import("../Components/Header"));
+export const App = React.lazy(() => import("../Components/App"));
+export const Room = React.lazy(() => import("../Components/Room"));
